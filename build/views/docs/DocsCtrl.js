fd6757dad72213b45f07eee785e65de9c55b1196
Sercan Eraslan <sercan.eraslan@sahibinden.com>
2014-12-18 09:21:28 +0200

    removed ignored file

--- build/views/docs/DocsCtrl.js
+++
@@ -1,6 +0,0 @@
-grabs.controller('DocsCtrl', ['$scope', '$routeParams', '$route', '$rootScope', function ($scope, $routeParams, $route, $rootScope) {
-    'use strict';
-
-    $rootScope.pageName = $route.current.name;
-    $scope.subPageName = $routeParams.page ? $routeParams.page : 'giris';
-}]);
--- build/views/homepage/HomepageCtrl.js
+++
@@ -1,5 +0,0 @@
-grabs.controller('HomepageCtrl', ['$route', '$rootScope', function ($route, $rootScope) {
-    'use strict';
-
-    $rootScope.pageName = $route.current.name;
-}]);
